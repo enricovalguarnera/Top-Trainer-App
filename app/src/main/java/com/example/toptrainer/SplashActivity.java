c868ec3a85df4a6f723306f86869b4dd276f108d
enrico valguarnera <enrico.valguarnera@eng.it>
2021-07-13 17:39:33 +0200

    Splash screen

--- app/src/main/java/com/example/toptrainer/SplashActivity.java
+++ app/src/main/java/com/example/toptrainer/SplashActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Date;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_splash);
+
+        // set timer
+        long startTime = System.currentTimeMillis();
+        long elapsedTime = 0L;
+
+        while (elapsedTime < 1000) {
+            //perform db poll/check
+            elapsedTime = (new Date()).getTime() - startTime;
+        }
+
         startActivity(new Intent(this, MainActivity.class));
         finish();
     }
+
 }
