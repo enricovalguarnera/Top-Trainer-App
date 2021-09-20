87dea46e90b8d1e638e480192446bd45fe9f1fbe
enrico valguarnera <enrico.valguarnera@eng.it>
2021-09-20 17:06:25 +0200

    AdMob integration

--- app/src/main/java/com/example/toptrainer/MainActivity.java
+++ app/src/main/java/com/example/toptrainer/MainActivity.java
@@ -1,6 +1,7 @@
 
 package com.example.toptrainer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -15,20 +16,98 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG_ADMOB = "---AD MOB----";
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+                createPersonalizeAd();
+            }
+        });
+
+    }
+
+    private void createPersonalizeAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        createInterstitialAd(adRequest);
+    }
+
+    public void createInterstitialAd(AdRequest adRequest) {
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+            new InterstitialAdLoadCallback() {
+                @Override
+                public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                    // The mInterstitialAd reference will be null until
+                    // an ad is loaded.
+                    mInterstitialAd = interstitialAd;
+                    Log.i(TAG_ADMOB, "onAdLoaded");
+
+                    mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                        @Override
+                        public void onAdDismissedFullScreenContent() {
+                            // Called when fullscreen content is dismissed.
+                            Log.d("TAG", "The ad was dismissed.");
+                            Intent intent = new Intent(MainActivity.this, TrainingActivity.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onAdFailedToShowFullScreenContent(AdError adError) {
+                            // Called when fullscreen content failed to show.
+                            Log.d("TAG", "The ad failed to show.");
+                        }
+
+                        @Override
+                        public void onAdShowedFullScreenContent() {
+                            // Called when fullscreen content is shown.
+                            // Make sure to set your reference to null so you don't
+                            // show it a second time.
+                            mInterstitialAd = null;
+                            Log.d("TAG", "The ad was shown.");
+                        }
+                    });
+                }
+
+                @Override
+                public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                    // Handle the error
+                    Log.i(TAG_ADMOB, loadAdError.getMessage());
+                    mInterstitialAd = null;
+                }
+            }
+        );
     }
 
     public void findTraining(View view) {
-        Intent intent = new Intent(this, TrainingActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.editText);
-//        String message = editText.getText().toString();
-        intent.putExtra("EXTRA_MESSAGE", "Messaggio di prova");
-        startActivity(intent);
+//
+        // Eseguo pubblicità AdMob
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(MainActivity.this);
+        } else {
+            Log.d(TAG_ADMOB, "The interstitial ad wasn't ready yet.");
+            Intent intent = new Intent(this, TrainingActivity.class);
+            startActivity(intent);
+        }
+
     }
 
     public void workInProgress (View view) {
